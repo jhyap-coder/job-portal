5c9a4d6e5573b05cc8ea13a5e119dabdfb139c4f
Chitra Bahadur Thapa Chhetri <geevanthapa750@gmail.com>
2026-03-03 13:10:54 +0545

    error solve

--- myProject/settings.py
+++ myProject/settings.py
@@ -18,7 +18,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # ------------------------------
 SECRET_KEY = os.environ.get("SECRET_KEY", "unsafe-default-secret")  # Provide in Render Environment
 
-DEBUG = os.environ.get("DEBUG", "False") == "True"
+DEBUG = os.environ.get("DEBUG", "True") == "True"
 
 ALLOWED_HOSTS = os.environ.get(
     "ALLOWED_HOSTS",
